04821dc33dec29d4142d23aa6529633ffe29ede1
Andre Ribas <zitoloco@gmail.com>
2021-02-15 14:45:30 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    indent: ['error', 2, { ignoredNodes: ['JSXElement'] }],
+    'react/jsx-indent': ['error', 2]
+  }
+}
